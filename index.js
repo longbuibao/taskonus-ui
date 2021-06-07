ea08b9320896774876fe74b327190014adf540ae
long bui <blong1102@gmail.com>
2021-06-07 14:37:13 +0700

    login sign up page

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const path = require('path')
 const app = express()
-const ui = require('./router/ui')
+
+const uiRouter = require('./router/ui')
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
+app.use(express.static(path.join(__dirname, '/public')))
 
-app.use(ui)
+
+app.use(uiRouter)
 
 app.listen(3001)
